20cff99742c8629839456182c7c2e85921dc10de
anupam.patel <anupam.patel@edgenetworks.in>
2021-08-15 13:24:38 +0530

    adding interactive loop for replay

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from app.tik_tac_toe import TicTakToe
 
 
 tc = TicTakToe()
-tc.generate_board()
-tc.display_board()
-tc.accept_players()
-tc.start()
-# tc.check_winner()
+choice = True
+while choice:
+    tc.generate_board()
+    tc.display_board()
+    tc.accept_players()
+    tc.start()
+    while True:
+        will_continue = input("Do you want to continue..(yes/no | y/n: ")
+        will_continue_lower = will_continue.lower()
+        if will_continue_lower in ["yes", "y"]:
+            print(f"Starting a new game....")
+            break
+        elif will_continue_lower in ["no", "n"]:
+            choice = False
+            print("Thanks for playing...")
+            break
+        else:
+            print("Please select any one of [ yes / no | y / n] ..")
